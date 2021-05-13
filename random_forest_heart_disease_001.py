4f87785824d6f6f926b9d999adc4dcf3c4ddbf2a
sarang-manohar <76211413+sarang-manohar@users.noreply.github.com>
2021-05-13 10:01:43 +0530

    trying binder

--- random_forest_heart_disease_001.py
+++ random_forest_heart_disease_001.py
@@ -6,6 +6,9 @@ import seaborn as sns
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split, GridSearchCV
 
+import warnings
+warnings.filterwarnings('ignore')
+
 #%%
 
 raw_data = pd.read_csv(r'C:\Users\saran\Desktop\up-skill\PGP - DSBA\Data Mining\US_Heart_Patients.csv')
@@ -24,4 +27,40 @@ cont_col = ['age','cigsPerDay','tot cholesterol','Systolic BP', 'Diastolic BP', 
 raw_data[cont_col].skew()
 # %%
 raw_data['Heart-Att'].value_counts(normalize = True)
+# %%
+raw_data.info()
+# %%
+# Initiate data imputation for missing values
+raw_data.Gender.value_counts()
+# %%
+raw_data.Gender.fillna('Female', inplace=True)
+# %%
+raw_data.age.fillna(raw_data.age.median(),inplace =True)
+# %%
+raw_data.education.value_counts()
+# %%
+raw_data.education.fillna(1.0, inplace=True)
+# %%
+raw_data[(raw_data['currentSmoker'] == 1)].info()
+# %%
+df_cig = raw_data[(raw_data['currentSmoker'] == 1)]
+# %%
+df_nocig = raw_data[(raw_data['currentSmoker'] == 0)]
+# %%
+df_cig.cigsPerDay.median()
+df_cig.cigsPerDay.fillna(20.0, inplace=True)
+# %%
+df_cig.info()
+df_nocig.info()
+
+df_nocig.cigsPerDay.unique()
+# %%
+df = pd.DataFrame.append(df_cig,df_nocig)
+# %%
+df.info()
+# %%
+df[(df['prevalentHyp'] == 1)]['BP Meds'].unique()
+
+df[(df['prevalentHyp'] == 1)]['BP Meds'].value_counts()
+
 # %%
