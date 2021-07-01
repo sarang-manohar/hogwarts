a042be8520d65cac92a12ed88b724afaf42672d4
sarang-manohar <76211413+sarang-manohar@users.noreply.github.com>
2021-07-01 08:54:44 +0530

    uploading Indegene case study

---
+++ academia/dash/dash0002.py
@@ -0,0 +1,73 @@
+# Import required libraries
+import pandas as pd
+import dash
+import dash_html_components as html
+import dash_core_components as dcc
+from dash.dependencies import Input, Output, State
+import plotly.graph_objects as go
+import plotly.express as px
+from dash import no_update
+
+
+# Create a dash application
+app = dash.Dash(__name__)
+
+# REVIEW1: Clear the layout and do not display exception till callback gets executed
+app.config.suppress_callback_exceptions = True
+
+# Read the airline data into pandas dataframe
+airline_data =  pd.read_csv('https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-DV0101EN-SkillsNetwork/Data%20Files/airline_data.csv', 
+                            encoding = "ISO-8859-1",
+                            dtype={'Div1Airport': str, 'Div1TailNum': str, 
+                                   'Div2Airport': str, 'Div2TailNum': str})
+
+
+# List of years 
+year_list = [i for i in range(2005, 2021, 1)]
+
+"""Compute graph data for creating yearly airline performance report 
+
+Function that takes airline data as input and create 5 dataframes based on the grouping condition to be used for plottling charts and grphs.
+
+Argument:
+     
+    df: Filtered dataframe
+    
+Returns:
+   Dataframes to create graph. 
+"""
+def compute_data_choice_1(df):
+    # Cancellation Category Count
+    bar_data = df.groupby(['Month','CancellationCode'])['Flights'].sum().reset_index()
+    # Average flight time by reporting airline
+    line_data = df.groupby(['Month','Reporting_Airline'])['AirTime'].mean().reset_index()
+    # Diverted Airport Landings
+    div_data = df[df['DivAirportLandings'] != 0.0]
+    # Source state count
+    map_data = df.groupby(['OriginState'])['Flights'].sum().reset_index()
+    # Destination state count
+    tree_data = df.groupby(['DestState', 'Reporting_Airline'])['Flights'].sum().reset_index()
+    return bar_data, line_data, div_data, map_data, tree_data
+
+
+"""Compute graph data for creating yearly airline delay report
+
+This function takes in airline data and selected year as an input and performs computation for creating charts and plots.
+
+Arguments:
+    df: Input airline data.
+    
+Returns:
+    Computed average dataframes for carrier delay, weather delay, NAS delay, security delay, and late aircraft delay.
+"""
+def compute_data_choice_2(df):
+    # Compute delay averages
+    avg_car = df.groupby(['Month','Reporting_Airline'])['CarrierDelay'].mean().reset_index()
+    avg_weather = df.groupby(['Month','Reporting_Airline'])['WeatherDelay'].mean().reset_index()
+    avg_NAS = df.groupby(['Month','Reporting_Airline'])['NASDelay'].mean().reset_index()
+    avg_sec = df.groupby(['Month','Reporting_Airline'])['SecurityDelay'].mean().reset_index()
+    avg_late = df.groupby(['Month','Reporting_Airline'])['LateAircraftDelay'].mean().reset_index()
+    return avg_car, avg_weather, avg_NAS, avg_sec, avg_late
+
+airline_data.head()
+
